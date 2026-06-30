1fcf96a1ff04607a38ee3b9841e771121a4e8427
hustcer <hustcer@outlook.com>
2026-06-30 19:25:33 +0800

    Deploy viewer examples from commit 9161b6f3 at 2026/06/30 19:25:33

--- viewer-examples/assets/svelte-Dc5aN5kO.js
+++
@@ -1,1 +0,0 @@
-import{n as e}from"./index-BFiIeJrZ.js";import{n as t,t as n}from"./types-WKkWu9NG.js";function r(t,n={}){let r=e(t,n);return n.onReady?.(r),{get viewer(){return r},update(n={}){return r.destroy(),r=e(t,n),n.onReady?.(r),r},destroy(){r.destroy()}}}var i=(e,i)=>{let a=r(e,t(i));return n(()=>a.viewer,()=>a.destroy())};export{i as mount};
