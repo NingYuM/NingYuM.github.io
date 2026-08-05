9130f89a07d0d009ed1f391c00ce410b13298772
hustcer <hustcer@outlook.com>
2026-08-05 09:21:58 +0800

    Deploy viewer examples from commit 91c71f11 at 2026/08/05 09:21:58

---
+++ viewer-examples/assets/vanilla-C9InBYcz.js
@@ -0,0 +1,1 @@
+import{n as e}from"./element-DK-18TYU.js";import"./index-Bu1tpxVf.js";import{n as t,t as n}from"./types-WKkWu9NG.js";var r=(r,i)=>{let a=e(r,t(i));return n(()=>a,()=>a.destroy())};export{r as mount};
--- viewer-examples/assets/vanilla-UV7ZsH_1.js
+++
@@ -1,1 +0,0 @@
-import{n as e}from"./element-CM34-hdz.js";import"./index-Cxg5PdIu.js";import{n as t,t as n}from"./types-WKkWu9NG.js";var r=(r,i)=>{let a=e(r,t(i));return n(()=>a,()=>a.destroy())};export{r as mount};
